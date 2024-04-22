ace76f1ad8fcf74b52cc86a9ace41e9fdf712056
LeeYeongin <youngin5757@gmail.com>
2024-04-22 18:04:43 +0900

    액션 태그

--- MustHaveJSP/src/main/java/common/Person.java
+++ MustHaveJSP/src/main/java/common/Person.java
@@ -2,6 +2,7 @@ package common;  // 기본 패키지 이외의 패키지(규약 1번)
 
 public class Person {
 	private String name;// private 멤버 변수(규약 2번)
+//	private String job;
 	private int age;	// private 멤버 변수(규약 2번)
 	
 	public Person() {} // 기본 생성자(규약 3번)
@@ -18,6 +19,12 @@ public class Person {
 	public void setName(String name) {
 		this.name = name;
 	}
+//	public String getJob() {
+//		return job;
+//	}
+//	public void setJob(String job) {
+//		this.job = job;
+//	}
 
 	public int getAge() {
 		return age;
