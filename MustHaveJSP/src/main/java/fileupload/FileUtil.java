faa1d854732524c7bab71c265768523dfa820043
LeeYeongin <youngin5757@gmail.com>
2024-05-07 18:19:25 +0900

    MVC 모델2 게시판 삭제, 편집 기능 추가

--- MustHaveJSP/src/main/java/fileupload/FileUtil.java
+++ MustHaveJSP/src/main/java/fileupload/FileUtil.java
@@ -106,6 +106,14 @@ public class FileUtil {
 			System.out.println("예외가 발생하였습니다.");
 			e.printStackTrace();
 		}
-		
+	}
+	
+	// 지정한 위치의 파일을 삭제합니다.
+	public static void deleteFile(HttpServletRequest req, String directory, String filename) {
+		String sDirectory = req.getServletContext().getRealPath(directory);
+		File file = new File(sDirectory + File.separator + filename);
+		if(file.exists()) {
+			file.delete();
+		}
 	}
 }
